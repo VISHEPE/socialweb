074236f3d04d1746eec8f41b42191e36f8429f76
VISHEPE <emmanuelodh428@gmail.com>
2024-09-24 15:15:37 -0700

    changes

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require_once 'config.php';
             <h1>Citizen Engagement Platform</h1>
             <nav class="main-nav">
                 <ul>
-                    <li><a href="index.html">Home</a></li>
+                    <li><a href="index.php">Home</a></li>
                     <li><a href="petitions.html">Petitions</a></li>
                     <li><a href="constitution.html">Constitution</a></li>
                     <li><a href="polls.html">Opinion Polls</a></li>
@@ -26,7 +26,20 @@ require_once 'config.php';
                 </ul>
             </nav>
         </div>
+
+
+
+
     </header>
+
+
+    <div class="container1">
+        <img src="vicky.png" alt="Placeholder Image" class="left-image">
+        <h1>Left Image Layout</h1>
+        <p>This example demonstrates how to place an image on the left side of a web page using HTML and CSS. The text wraps around the image, creating a natural flow in the layout.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
 
     
 
